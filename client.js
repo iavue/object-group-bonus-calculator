543d7bd45af794bf89134860b20e3598139a75c3
Ia Vue <iavue00@gmail.com>
2023-02-21 13:42:41 -0600

    completed assignment

--- client.js
+++ client.js
@@ -62,20 +62,87 @@ loopThroughArray(employees)
 // 
 function calculateIndividualEmployeeBonus( employee ) {  // this employee parameter is an object that is passed in to
   // your logic here
-  let newObj = {};
-  let name = employee.name; // the name property of the employee object
+  let newObj = {}; // declared newObject variable and set it to empty
+  let name = employee.name; // declared name variable and set it to the name property of the employee object
   newObj.name = name; // we're adding a name property to the newObj object and assigning it to the value of employee object's name property's value
   
   let bonus = 0;
-  if ( employee.reviewRating <= 2 ) {
-    let bonus = 0;
-  }
+  let bonusPercent = 0;
   
+  if ( employee.reviewRating === 5 ) {
+    bonusPercent += 0.10;
+    console.log('Rating is 5, so bonus percent is:', bonusPercent);
+  }
+  else if ( employee.reviewRating === 4 ) {
+    bonusPercent += 0.06;
+    console.log('Rating is 4, so bonus percent is:', bonusPercent);
+  }
+  else if ( employee.reviewRating === 3 ) {
+    bonusPercent += 0.04;
+    console.log('Rating is 3, so bonus percent is:', bonusPercent);
+  }
+  else {
+    console.log('Rating is 2 or below, so bonus percent is:', bonusPercent);
+  }
+  //Checking if employee number is 4 digits
+    //If it is, add 5% to bonus percent
+  if( employee.employeeNumber.length === 4 ) {
+    bonusPercent += 0.05;
+    console.log('Employee number is 4 digits, so bonus percent is:', bonusPercent);
+  } else {
+    console.log( 'Employee number is not 4 digits, so bonus percent is unchanged' );
+  }
+
+  //Checking if Annual Income is above $65000
+    //If it is, subtract 1%
+    //Since this is the only condition that can bring 'bonusPercent' below 0, added an && statement
+  if( Number(employee.annualSalary) > 65000 && bonusPercent > 0) {
+    bonusPercent -= 0.01;
+    console.log('Annual salary is above $65000, so bonus percent is:', bonusPercent);
+  } else {
+    console.log( 'Annual salary is less than $65000 or bonusPercent is 0, so bonus percent is unchanged' );
+  }
+
+  //No bonus can be above 13%
+  if( bonusPercent > 0.13) {
+    bonusPercent = 0.13;
+    console.log('Bonus is above 13%, so bonus percent is:', bonusPercent);
+  }
+
+  //Testing bonusPercent amount
+  console.log( 'Total Bonus percent is:', bonusPercent );
+  
+  //Adding bonusPercentage property to object
+  employee.bonusPercentage = bonusPercent;
+  console.log( employee.bonusPercentage );
+
+  //Assigning dollar value from calculation to 'bonus'
+  bonus = bonusPercent * Number(employee.annualSalary);
+  console.log( bonus );
+
+  // Added totalCompensation property to object
+  employee.totalCompensation = Number(employee.annualSalary) + bonus; // Used Number() to convert string to number
+  console.log( 'Total compensation is:', employee.totalCompensation);
+
+  
+  employee.totalBonus = Math.round(bonus); // Used Math.round() to round to nearest dollar
+  console.log('Total bonus:', bonus);
+
   // return new object with bonus results
   return newObj;
 }
 
 // test to make sure the name is added 
+console.log( 'Atticus' );
 console.log(calculateIndividualEmployeeBonus(employees[0]));
+console.log( 'Jem' );
+console.log(calculateIndividualEmployeeBonus(employees[1]));
+console.log( 'Scout' );
+console.log(calculateIndividualEmployeeBonus(employees[2]));
+console.log( 'Robert' );
+console.log(calculateIndividualEmployeeBonus(employees[3]));
+console.log( 'Mayella' );
+console.log(calculateIndividualEmployeeBonus(employees[4]));
+
 
 // console.log(employees[0].name)
